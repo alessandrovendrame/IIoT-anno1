6a607c103a5e6e31f37750bb2b07ec8e2a2c0aa4
alessandrovendrame <60774642+alessandrovendrame@users.noreply.github.com>
2020-02-07 16:59:12 +0100

    per te Alvi

---
+++ Python/20200207/Ratti.py
@@ -0,0 +1,42 @@
+import time
+import random
+import statistics
+
+#CONSTANTS
+
+GOAL = 50000
+NUM_RATS = 20
+INITIAL_MIN_WT = 200
+INITIAL_MAX_WT = 600
+INITIAL_MODE_WT = 300
+MUTATE_ODDS = 0.01
+MUTATE_MIN = 0.5
+MUTATE_MAX = 1.2
+LITTER_SIZE = 8
+LITTERS_PER_YEAR = 10
+GENERATION_LIMIT = 500
+
+#ensure even-number of rats for breeding pairs:
+
+if NUM_RATS % 2 != 0:
+    NUM_RATS += 1
+
+def populate(num_rats, min_wt, max_wt, mode_wt):
+    """Initialize a population with a triangular distribution of weights."""
+    return [int(random.triangular(min_wt, max_wt, mode_wt)) for i in range(num_rats)]
+
+def fitness(population, goal):
+    """Measure population fitness based on an attrubute mean vs target."""
+    ave = statistics.mean(population)
+    return ave/goal
+
+def select(population, to_retain):
+    """Cull a population to retain only a specified number of members."""
+    sorted_population = sorted(population)
+    to_retain_by_sex = to_retain//2
+    members_per_sex = len(sorted_population)//2
+    females = sorted_population[:members_per_sex]
+    males = sorted_population[members_per_sex:]
+    selected_females = females[-to_retain_by_sex:]
+    selected_males = males[-to_retain_by_sex:]
+    return selected_males, selected_females
